6de7ce71d5633ea48485e8bf7c03a5d2b6c7d357
영현 <yh97yhyh@naver.com>
2024-06-23 22:30:07 +0900

    binary search

---
+++ ProblemSolving/Baekjoon/BinarySerach/1300.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  ProblemSolving
+//
+//  Created by 영현 on 6/20/24.
+//
+
+// K번째 수
+// 1300
+// A[i][j] = i * j
+
+import Foundation
+
+let n = Int(readLine()!)! // n * n 배열
+let k = Int(readLine()!)! // k번째 인덱스 arr[k] (오름차순 정렬)
+
+func binarySearch() -> Int {
+    var start = 1
+    var end = n * n
+    
+    while true {
+        if start >= end {
+            break
+        }
+        
+        let mid = (start + end) / 2
+        var count = 0
+        
+        for i in 1...n {
+            count += min(mid / i, n) // 최대 n개여야 함
+        }
+        
+        if count >= k {
+            end = mid
+        } else {
+            start = mid + 1
+        }
+        
+    }
+    
+    return start
+}
+
+print(binarySearch())
