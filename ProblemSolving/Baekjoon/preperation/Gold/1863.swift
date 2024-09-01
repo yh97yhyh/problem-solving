27287f26ec84ba8f4fea900f60ff5b374cddaf57
영현 <yh97yhyh@naver.com>
2024-09-01 22:25:18 +0900

    baekjoon - 1863

---
+++ ProblemSolving/Baekjoon/preperation/Gold/1863.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  ProblemSolving
+//
+//  Created by 영현 on 9/1/24.
+//
+
+// 1863
+// 스카이라인
+// 건물의 높이가 높아지면 그 뒤에 하나의 건물이 더 있다
+// 건물의 높이가 낮아지면 뒤에 있는 건물이 끝났다
+
+import Foundation
+
+let n = Int(readLine()!)!
+var skylines: [Int] = []
+var answer = 0
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let y = input[1]
+    skylines.append(y)
+}
+
+skylines.append(0) // 마지막 건물을 세기 위해
+var stack = [0] // 스택이 비지 않게
+
+for p in skylines {
+    var height = p // 마지막으로 카운트한 건물의 높이를 기록하고, 앞에서 카운트한 건물을 또 카운트하지 않도록 함
+    while true {
+        if stack.last! <= p {
+            break
+        }
+        if stack.last != height {
+            answer += 1
+            height = stack.last!
+        }
+        stack.removeLast()
+    }
+    stack.append(p)
+}
+
+print(answer)
+
+//var arr = Array(repeating: Array(repeating: 0, count: 5000001), count: 1000001)
+//var checked = Array(repeating: Array(repeating: false, count: 5000001), count: 1000001)
+//
+//var preX = 0
+//var preY = 0
+//var maxX = 0
+//var maxY = 0
+//
+//for _ in 0..<n {
+//    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+//    let curX = input[0]
+//    let curY = input[1]
+//    
+//    for x in preX..<curX {
+//        for y in 0...preY {
+//            arr[x][y] = 1
+//        }
+//    }
+//    
+//    preX = curX
+//    preY = curY
+//    maxX = curX
+//    maxY = max(maxY, curY)
+//}
+//
+//for y in 0...preY {
+//    arr[maxX][y] = 1
+//}
