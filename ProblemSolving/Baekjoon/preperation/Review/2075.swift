ce537cf5343d10ccb4091fcde4841281dfd8e1a0
영현 <yh97yhyh@naver.com>
2024-09-18 19:37:55 +0900

    baekjoon - 2075

---
+++ ProblemSolving/Baekjoon/preperation/Review/2075.swift
@@ -0,0 +1,76 @@
+//
+//  main.swift
+//  ProblemSolving
+//
+//  Created by 영현 on 9/17/24.
+//
+
+// N번째 큰 수
+// 2075
+// https://my-coding-notes.tistory.com/556
+
+import Foundation
+
+let file = FileIO()
+
+let n = file.readInt()
+var arr: [Int] = []
+
+for _ in 0..<n*n {
+    let num = file.readInt()
+    arr.append(num)
+}
+
+arr.sort()
+print(arr[n*n-n])
+
+final class FileIO {
+    private let buffer: Data
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        self.buffer = try! fileHandle.readToEnd()! // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        guard index < buffer.count else { return 0 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
